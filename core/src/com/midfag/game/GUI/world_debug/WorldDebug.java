5bee3eebf52ea459a898216f3207a92d89ce4ff3
NoSimpleWay <36441257+NoSimpleWay@users.noreply.github.com>
2018-06-22 17:02:20 +0300

    Revert "eclipse project setting"
    This reverts commit 362126d4c1cc2c6d0e6371bc1162b469f9a719e7.

--- core/src/com/midfag/game/GUI/world_debug/WorldDebug.java
+++ core/src/com/midfag/game/GUI/world_debug/WorldDebug.java
@@ -1,12 +1,11 @@
 package com.midfag.game.GUI.world_debug;
 
-import com.badlogic.gdx.graphics.Color;
 import com.midfag.game.GScreen;
 import com.midfag.game.Main;
 
 public class WorldDebug {
 	
-	float debug_text_x=400;
+	float debug_text_x=200;
 	float debug_text_y=30;
 	
 	//String default_text="";
@@ -18,7 +17,7 @@ public class WorldDebug {
 	
 	public void draw(float _d)
 	{
-		GScreen.draw_shaded_text(get_debug_text(), debug_text_x, debug_text_y,Color.WHITE,450);
+		Main.font.draw(GScreen.batch_static, get_debug_text(), debug_text_x, debug_text_y);
 	}
 	
 	public String get_debug_text()
